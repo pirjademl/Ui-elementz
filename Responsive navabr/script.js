024afbaaad61d84fb3b435e3e0749980df4b683d
unknown <pirjademl7@gmail.com>
2023-10-26 00:16:48 +0530

    Resonsive navbar

---
+++ Responsive navabr/script.js
@@ -0,0 +1,5 @@
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
